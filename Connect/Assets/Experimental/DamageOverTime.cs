373415cb1cf08ec96c89b5efb7a0b7f55ca93fe1
DavidYeLuo <dayeluo@galileoweb.org>
2019-07-19 08:00:37 -0700

    Update 7.19
    + DamageOverTime happens every second instead of every frame.

--- Connect/Assets/Experimental/DamageOverTime.cs
+++ Connect/Assets/Experimental/DamageOverTime.cs
@@ -6,17 +6,26 @@ public class DamageOverTime : BaseEffect
 {
     public int damage;
 
+    private Cooldown nextTick;
     public DamageOverTime(GameObject obj) : base()
     {
         // Grabs obj's component
+
+        nextTick = new Cooldown(0);
     }
 
     public override void ApplyEffect()
     {
-        Debug.Log("You got hit " + damage);
+        if(!nextTick.isOnCD())
+        {
+            Debug.Log("You got hit " + damage); // Hit
+
+            nextTick.NextCD(1);
+        }
     }
 
     public override void EndEffect()
     {
+        nextTick.NextCD(0);
     }
 }
